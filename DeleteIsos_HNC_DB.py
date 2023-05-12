e4f7e892dd7a05101ce7c3d016a952e36a19dd96
ElenaBorderias <elenabv29@gmail.com>
2023-05-12 14:17:03 +0200

    Update DeleteIsos_HNC_DB.py

--- DeleteIsos_HNC_DB.py
+++ DeleteIsos_HNC_DB.py
@@ -13,8 +13,10 @@ ct_name= 'CT 1'
 roi_names = [x.OfRoi.Name for x in case.PatientModel.StructureSets[ct_name].RoiGeometries]
 
 for roi in roi_names:
-  if 'isodose' in roi or 'iso_np' in roi:
+  if 'isodose' in roi or 'iso_np' in roi or 'iso_p_np' in roi:
     try:
         case.PatientModel.RegionsOfInterest[roi].DeleteRoi()
     except:
        print('I could not delete this ROI ', roi)
+
+patient.Save()
