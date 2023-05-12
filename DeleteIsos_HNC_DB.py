2d2c0aa2b72b67ae30aaa05d9352b1014a3fc9dd
ElenaBorderias <elenabv29@gmail.com>
2023-05-12 11:49:37 +0200

    add HNC delete isos script

---
+++ DeleteIsos_HNC_DB.py
@@ -0,0 +1,20 @@
+# Script recorded 28 Feb 2023, 13:13:29
+
+#   RayStation version: 13.10.0.7342
+#   Selected patient: ...
+
+from connect import *
+
+case = get_current("Case")
+patient = get_current("Patient")
+
+ct_name= 'CT 1'
+
+roi_names = [x.OfRoi.Name for x in case.PatientModel.StructureSets[ct_name].RoiGeometries]
+
+for roi in roi_names:
+  if 'isodose' in roi or 'iso_np' in roi:
+    try:
+        case.PatientModel.RegionsOfInterest[roi].DeleteRoi()
+    except:
+       print('I could not delete this ROI ', roi)
